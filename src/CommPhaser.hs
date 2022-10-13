4100dda1f906cc900245527207887d6dfcd526db
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-13 17:14:30 +0700

    add half function to CreateCommParser

--- src/CommPhaser.hs
+++ src/CommPhaser.hs
@@ -1,40 +1,71 @@
 module CommPhaser (
-    commPhase, Comm, ManComm, QueComm
+    commParse, Comm, ManComm, QueComm
 ) where
 
     import Tokenizer as Tk
-    import TaskModel
+    import TaskModel as Tm
+    import PtInTime
 
-    data Comm = Manipulate ManComm | Query QueComm| NotComm
+    data Comm = Manipulate ManComm | Query QueComm | NonComm
         deriving (Show)
     data ManComm = CrComm Task | EdComm ParTask ParTask | DelComm ParTask
         deriving (Show)
     data QueComm = ShComm ParTask 
         deriving (Show)
 
-    commPhase:: Tokens -> Comm
-    commPhase [] = NotComm
-    commPhase (x:xs) 
-            | x == "CREATE" = cComPhaser xs
-            | x == "EDIT" = NotComm
-            | x == "DELECTE" = NotComm
-            | x == "SHOW" = NotComm
-            | otherwise = NotComm
+    commParse:: Tokens -> Either String Comm
+    commParse [] = Left "empty tokens"
+    commParse (x:xs) 
+            | x == "CREATE" = createCommParse xs
+            | x == "EDIT" = editCommParse xs
+            | x == "DELECTE" = delCommParse xs
+            | x == "SHOW" = showCommParse xs
+            | otherwise = Left "unknown command"
 
-    cComPhaser:: Tokens -> Comm
-    cComPhaser tokens = let 
-        tags = tagFinder tokens
-        maybDue = dueDateFinder tokens
-        maybRemi = reminFinder tokens
-        desc = Tk.tokensToStr tokens
-        in (Manipulate(CrComm (False, tags, desc, maybDue, maybRemi)))
+    createCommParse:: Tokens -> Either String Comm
+    createCommParse ts = 
+        case (semiColonD ts) of
+        Nothing -> Right ( Manipulate( CrComm (descToTask (tokensToStr ts))))
+        Just (desTs, dNr) -> either 
+            (\x -> Left x :: Either String Comm) 
+            (\x -> Right (cCPAux (tokensToStr desTs) x)) 
+            (dueNReParse dNr)
     
-    tagFinder:: Tokens -> [String]
-    tagFinder tokens = foldl tagFinderAux [] tokens
+    cCPAux::String -> Either DnRobj (DnRobj,DnRobj) -> Comm
+    cCPAux des bck = let
+        task = case bck of
+            Left o1 -> taskfrom1Dnr des o1
+            Right (o1,o2) -> taskfrom2Dnr des o1 o2
+        in Manipulate( CrComm (task))
 
-    tagFinderAux::[String] -> Token ->[String]
-    tagFinderAux acc (x:xs) = if x == '#' then xs:acc else acc
-    tagFinderAux acc [] = acc
+    semiColonD:: Tokens -> Maybe (Tokens, Tokens)
+    semiColonD ts = aux [] ts
+        where aux _ [] = Nothing
+              aux acc (x:xs) = if x == ";" then Just (acc++[x],xs) else aux (acc++[x]) xs
+
+    dueNReParse:: Tokens -> Either String (Either DnRobj (DnRobj,DnRobj))
+    dueNReParse _ = Left "Temp"
+    -- dueNReParse ts = let
+    --     ei = case (semiColonD ts) of
+    --             Nothing -> 
+    --             Just (lf, rt) -> (dNrParse lf,)
+
+    editCommParse:: Tokens -> Either String Comm
+    editCommParse _ = Left "Temp"
+
+    delCommParse:: Tokens -> Either String Comm
+    delCommParse _ = Left "Temp"
+
+    showCommParse:: Tokens -> Either String Comm
+    showCommParse _ = Left "Temp"
+
+    
+    -- tagFinder:: Tokens -> [String]
+    -- tagFinder tokens = foldl tagFinderAux [] tokens
+
+    -- tagFinderAux::[String] -> Token ->[String]
+    -- tagFinderAux acc (x:xs) = if x == '#' then xs:acc else acc
+    -- tagFinderAux acc [] = acc
 
     dueDateFinder x = Nothing
 
