11f5fb1be972045a8f6c8ca1d5c2a43a83df3dab
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-15 18:05:02 +0700

    AuxFunc: Add Divine Tokens with String

--- src/AuxFunc.hs
+++ src/AuxFunc.hs
@@ -1,9 +1,12 @@
 module AuxFunc (
     toLowerStr,
-    strIsNum
+    strIsNum,
+    semiColonD,
+    stringD
 ) where
 
     import Data.Char (toLower, isDigit)
+    import Tokenizer (Tokens, Token)
 
     toLowerStr:: String -> String
     toLowerStr = foldl (\acc x -> acc ++ [toLower x]) ""
@@ -12,3 +15,11 @@ module AuxFunc (
     strIsNum []  = False
     strIsNum tok = foldl aux True tok
         where aux acc x = if isDigit x then acc && True else False
+
+    semiColonD:: Tokens -> Maybe (Tokens, Tokens)
+    semiColonD ts = stringD ";" ts
+
+    stringD:: String -> Tokens -> Maybe (Tokens, Tokens)
+    stringD strD ts = aux [] ts
+        where aux _ [] = Nothing
+              aux acc (x:xs) = if x == strD then Just (acc++[x],xs) else aux (acc++[x]) xs
