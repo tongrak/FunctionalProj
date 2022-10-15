8c85c06d0a11bd60042e6a91ccf775a6b753ad97
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-15 18:08:24 +0700

    PtInTime: dNrParse, dueDParse, and reminDParse imp

--- src/PtInTime.hs
+++ src/PtInTime.hs
@@ -6,6 +6,7 @@ module PtInTime (
     -- import Data.Time.Calendar (MonthOfYear, Year)
     import Tokenizer
     import TaskModel
+    import AuxFunc
 
     type Min = Int
     type Hou = Int
@@ -18,8 +19,32 @@ module PtInTime (
 
     type PtOnClen  = (Date, MonthNum, YearNum)
 
-    dNrParse::Tokens -> DnRobj
-    dNrParse _ = (0,0,0)
+    dNrParse::Tokens -> Either String DnRobj
+    dNrParse [] = Left "dNrParse: Empty Tokens"
+    dNrParse (x:xs)
+        | x == "BY" = Left $ dueDParse xs
+        | x == "REMIND" = Right $ 
+        | otherwise = Left "Unknown Point In Time"
+
+    dueDParse::Tokens -> Either String PtOnClen
+    dueDParse [] = Left "dueDParse: Empty Tokens"
+    dueDParse ts = case stringD ":" ts of
+        Nothing -> Right dayOfWeekP (tokensToStr ts)
+        Just (fh, lh) -> Right (tkToNum fh, tkToNum lh, 2022)
+        where tkToNum = strIsNum . tokensToStr
+
+    dayOfWeekP:: String -> PtOnClen
+    dayOfWeekP _ = (1,1,1)
+
+    -- ptOnClenP::
+
+    reminDParse::Tokens -> Either String (PtOnClock, PtOnClen)
+    reminDParse ts = case stringD "h" ts of
+        Nothing -> Left "reminDParse: Invalid form"
+        Just (fh, lh) -> Right (ptOnClkP fh,dueDParse lh)
+
+    ptOnClkP::String -> PtOnClock
+    ptOnClkP _ = (1,1)
 
     -- TODO:
     -- clkPhase:: String -> Maybe PtOnClock 
