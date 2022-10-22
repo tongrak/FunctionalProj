74ebede15d1bf16a06c20de02c772bd450cd328a
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-22 23:26:28 +0700

    Tokenizer: whSpace and semi bug fix

--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -12,16 +12,17 @@ module Tokenizer (
 
     tokenize::String -> Tokens
     tokenize [] = []
-    tokenize list = foldl tokenizeAux [] $ reverse list
+    tokenize str = filter (/= "") $ foldl tokenizeAux [] $ reverse str
 
     tokenizeAux:: [[Char]] -> Char -> [[Char]]
     tokenizeAux list inA 
-        | isSpace  inA  = []:list
+        | isSpace inA   = []:list
+        | inA == ';'    = []:";":list
         | otherwise     = addCLats list 
         where 
             addCLats (x:xs) = (inA:x):xs
             addCLats [] = [[inA]]
 
     tokensToStr:: Tokens -> String
-    tokensToStr tokens = foldl1 (++) tokens
-
+    tokensToStr tokens = foldl1 aux tokens
+        where aux acc x = acc ++ " " ++ x
