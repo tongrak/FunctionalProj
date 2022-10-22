3461f49e9d2c0e45f3710721f6c2725920f3ab30
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-22 23:24:49 +0700

    TaskModel: Change var name

--- src/TaskModel.hs
+++ src/TaskModel.hs
@@ -1,18 +1,19 @@
 module TaskModel(
-    Task, ParTask, DnRobj, descToTask, taskfrom1Dnr, taskfrom2Dnr
+    Task, ParTask, DnRobj, DueDate, Reminder, descToTask, taskfrom1Dnr, taskfrom2Dnr
 ) where
 
-    import PtInTime
+    type Date = String
+    type Month = String
+    type MiliTime = String
 
-    type Reminder = (PtOnClock, PtOnClen)
-    type DueDate = PtOnClen
-    -- type Tags = [[Char]]
+    type Reminder = (MiliTime, DueDate)
+    type DueDate = (Date, Month)
     type Desc = String
     type Marked = Bool
 
     type DnRobj = Either DueDate Reminder
 
-    data Task =  TaskCon 
+    data Task =  Task 
         {   mark::Bool,
             desc::String,
             fstDnR::Maybe DnRobj,
@@ -27,14 +28,10 @@ module TaskModel(
         } deriving (Show)
 
     descToTask::String -> Task
-    descToTask str = TaskCon False str Nothing Nothing
+    descToTask str = Task False str Nothing Nothing
 
     taskfrom1Dnr::String -> DnRobj -> Task
-    taskfrom1Dnr str dnr = TaskCon False str (Just dnr) Nothing
+    taskfrom1Dnr str dnr = Task False str (Just dnr) Nothing
 
     taskfrom2Dnr::String -> DnRobj -> DnRobj -> Task
-    taskfrom2Dnr str dnr1 dnr2 = TaskCon False str (Just dnr1) (Just dnr2)
-
-
-    -- type Task = (Marked, Desc, (Maybe DueDate), (Maybe Reminder)) 
-    -- type ParTask = ((Maybe Marked), (Maybe Desc), (Maybe DueDate), (Maybe Reminder))
+    taskfrom2Dnr str dnr1 dnr2 = Task False str (Just dnr1) (Just dnr2)
